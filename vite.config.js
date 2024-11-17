4d1fb428e696637a7faaee2fd2eccd3aa2f822a9
unknown <kamrunnesasamiya1997@gmail.com>
2024-11-17 22:18:44 +0600

    made some more new changes

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,16 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'public/assets' // Output files will be in 'public/assets'
-  }
+  },
+
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
 })
